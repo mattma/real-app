dbf25945f5884ddadfc7fb5212eb86a524e44fec
Matt Ma <bigmabig@gmail.com>
2016-10-28 23:14:55 -0700

    show action bar with light styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { topmost } from "ui/frame";
+import { isIOS } from "platform";
 
 @Component({
-  selector: 'ns-app',
-  template: '<page-router-outlet></page-router-outlet>'
+  selector: 'real-app',
+  template: `
+    <ActionBar class="action-bar-dark">
+      <ActionItem ios.position="left">
+        <Button text="&#xf007;" class="fa"></Button>
+      </ActionItem>
+      <StackLayout>
+        <Label text="Real" class="title"></Label>
+      </StackLayout>
+      <ActionItem ios.position="right" android.position="popup">
+        <Button text="&#xf004;" class="fa"></Button>
+      </ActionItem>
+    </ActionBar>
+    
+    <page-router-outlet></page-router-outlet>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit () {
+    // if iOS, show status bar color in white
+    if (isIOS) {
+      let navigationBar = topmost().ios.controller.navigationBar;
+      navigationBar.barStyle = 1;
+    }
+  }
 }
