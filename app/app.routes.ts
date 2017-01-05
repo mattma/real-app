5da7c32316c6bb475b7cccc051bafcad10925c6f
Matt Ma <bigmabig@gmail.com>
2017-01-04 22:34:37 -0800

    update the sub nav tabview styles

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 
 import { TrainingComponent } from './components/training/training.component';
 import { LoginComponent } from './components/login/login.component';
+import { DiscoverComponent } from './components/discover/discover.component';
 
 // Routes Definition
 // import NotFound404Routes from './+not-found-404/not-found-404.routes';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/training' },
+  { path: '', pathMatch: 'full', redirectTo: '/discover' },
   { path: 'login', component: LoginComponent },
   { path: 'training', component: TrainingComponent },
+  { path: 'discover', component: DiscoverComponent },
   // catch all route must be the last one defined
   // ...NotFound404Routes
 ];
